2e2748a31ab7d426865e4b5dde36a849a936eb72
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-05-10 14:48:34 +0300

    access right

---
+++ show_order_line/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Show Order Line',
+    'category': 'Sales/Purchase',
+    'sequence': 1,
+    'version': '15.0.1',
+    'license': 'LGPL-3',
+    'summary': """Show Sale Orders Line And Purchase Lines""",
+    'description': "",
+    'author': 'Ibrahim Kaddour',
+    'depends': ['sale', 'purchase'],
+    'data': [
+        'menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
