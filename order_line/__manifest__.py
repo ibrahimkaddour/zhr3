c6294fa310a8c3e29c6343cfb32d5381f0ac7117
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-05-16 09:22:25 +0300

    order line

---
+++ order_line/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Order Line',
+    'version': '1.0',
+    'depends': ['purchase', 'sale'],
+    'category': 'test',
+    'data': [
+        'views/menu.xml',
+
+    ],
+    'assets': {
+
+    },
+    'installable': True,
+    'application': False,
+    "license": "LGPL-3",
+}
