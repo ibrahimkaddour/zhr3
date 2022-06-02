0704636848fc94882ba585c49a3c1824291000c8
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-06-02 15:41:18 +0300

    active po

---
+++ zhr_purchase_customizations/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ZHR Purchase Customizations",
+    'summary': """
+         ZHR Purchase Customizations""",
+    'description': """
+        ZHR Purchase Customizations
+    """,
+    'author': "AMCL",
+    'version': '15.0.1',
+    'license': 'LGPL-3',
+    'depends': ['account', 'purchase'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/purchase.xml',
+    ],
+
+
+}
