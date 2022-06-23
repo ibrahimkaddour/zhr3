8ae39d67ba558a54c2edffa85a085cc984013b76
ibrahimkaddour <ibrahimkaddour@gmail.com>
2022-06-23 08:47:51 +0300

    generate code on variant

---
+++ zhr_generate_code/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'ZHR Project - Generate Code',
+    'category': 'Product',
+    'sequence': 1,
+    'version': '15.0.1',
+    'license': 'LGPL-3',
+    'summary': """KH Project - Customizations - Generate Internal Reference On Variant""",
+    'description': """KH Project - Customizations - Generate Internal Reference On Variant""",
+    'author': 'Ibrahim Kaddour',
+    'depends': ['product'],
+    'data': [
+        'security/groups.xml',
+        'views/product.xml',
+
+    ],
+    'installable': True,
+    'application': True,
+}
+
---
+++ zhr_generate_code/models/product.py
@@ -0,0 +1,141 @@
+import itertools
+import logging
+from odoo import api, fields, models, tools, _, SUPERUSER_ID
+from odoo.exceptions import ValidationError, RedirectWarning, UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    is_seq_readonly = fields.Boolean(default=False)
+    seq_from = fields.Char('From')
+    seq_to = fields.Char('To')
+
+    variant_ids = fields.One2many(
+        comodel_name='product.product', inverse_name='product_tmpl_id', string='Variant')
+
+    def _create_variant_ids(self):
+        self.flush()
+        Product = self.env["product.product"]
+
+        variants_to_create = []
+        variants_to_activate = Product
+        variants_to_unlink = Product
+
+        for tmpl_id in self:
+            lines_without_no_variants = tmpl_id.valid_product_template_attribute_line_ids._without_no_variant_attributes()
+
+            all_variants = tmpl_id.with_context(active_test=False).product_variant_ids.sorted(
+                lambda p: (p.active, -p.id))
+
+            current_variants_to_create = []
+            current_variants_to_activate = Product
+
+            # adding an attribute with only one value should not recreate product
+            # write this attribute on every product to make sure we don't lose them
+            single_value_lines = lines_without_no_variants.filtered(
+                lambda ptal: len(ptal.product_template_value_ids._only_active()) == 1)
+            if single_value_lines:
+                for variant in all_variants:
+                    combination = variant.product_template_attribute_value_ids | single_value_lines.product_template_value_ids._only_active()
+                    # Do not add single value if the resulting combination would
+                    # be invalid anyway.
+                    if (
+                            len(combination) == len(lines_without_no_variants) and
+                            combination.attribute_line_id == lines_without_no_variants
+                    ):
+                        variant.product_template_attribute_value_ids = combination
+
+            # Set containing existing `product.template.attribute.value` combination
+            existing_variants = {
+                variant.product_template_attribute_value_ids: variant for variant in all_variants
+            }
+
+            # Determine which product variants need to be created based on the attribute
+            # configuration. If any attribute is set to generate variants dynamically, skip the
+            # process.
+            # Technical note: if there is no attribute, a variant is still created because
+            # 'not any([])' and 'set([]) not in set([])' are True.
+            if not tmpl_id.has_dynamic_attributes():
+                # Iterator containing all possible `product.template.attribute.value` combination
+                # The iterator is used to avoid MemoryError in case of a huge number of combination.
+                all_combinations = itertools.product(*[
+                    ptal.product_template_value_ids._only_active() for ptal in lines_without_no_variants
+                ])
+                # For each possible variant, create if it doesn't exist yet.
+                for combination_tuple in all_combinations:
+                    combination = self.env['product.template.attribute.value'].concat(*combination_tuple)
+                    if combination in existing_variants:
+                        current_variants_to_activate += existing_variants[combination]
+                    else:
+                        current_variants_to_create.append(tmpl_id._prepare_variant_values(combination))
+                        if len(current_variants_to_create) > 999999:
+                            raise UserError(_(
+                                'The number of variants to generate is too high. '
+                                'You should either not generate variants for each combination or generate them on demand from the sales order. '
+                                'To do so, open the form view of attributes and change the mode of *Create Variants*.'))
+                variants_to_create += current_variants_to_create
+                variants_to_activate += current_variants_to_activate
+
+            else:
+                for variant in existing_variants.values():
+                    is_combination_possible = self._is_combination_possible_by_config(
+                        combination=variant.product_template_attribute_value_ids,
+                        ignore_no_variant=True,
+                    )
+                    if is_combination_possible:
+                        current_variants_to_activate += variant
+                variants_to_activate += current_variants_to_activate
+
+            variants_to_unlink += all_variants - current_variants_to_activate
+        if variants_to_activate:
+            variants_to_activate.write({'active': True})
+        if variants_to_create:
+            Product.create(variants_to_create)
+        if variants_to_unlink:
+            variants_to_unlink._unlink_or_archive()
+
+        # prefetched o2m have to be reloaded (because of active_test)
+        # (eg. product.template: product_variant_ids)
+        # We can't rely on existing invalidate_cache because of the savepoint
+        # in _unlink_or_archive.
+        self.flush()
+        self.invalidate_cache()
+        return True
+
+    @api.model
+    def create(self, vals):
+        res = super(ProductTemplate, self).create(vals)
+
+        if vals['seq_from']:
+            res.seq_to = int(vals['seq_from']) + 999999
+            seq = int(vals['seq_from'])
+            for variant in res.variant_ids:
+                variant.write({'default_code': seq})
+                seq += 1
+            res.is_seq_readonly = True
+        return res
+
+    def write(self, vals):
+        res = super(ProductTemplate, self).write(vals)
+        # _logger.critical('--*************--')
+
+        code = self.env['product.product'].search([
+            ('product_tmpl_id', '=', self.id), ('active', '=', False)], limit=1, order='default_code DESC').default_code
+        if code:
+            seq = int(code) + 1
+        else:
+            if self.seq_from:
+                seq = int(self.seq_from)
+        if self.variant_ids:
+            for line in self.variant_ids:
+                # if str(seq) in variants_archived:
+                #     seq += 1
+                #     continue
+                line.write({'default_code': seq})
+                seq += 1
+
+            # raise
+        return res
