a6c2c1aca5cdcee3dabcc3bcaacab475fec0c134
Aneesh AV <aneesh@amcde.com>
2022-06-28 16:18:08 +0300

    [IMP]BOM Updated

--- amcl_bom_customizations/models/product.py
+++ amcl_bom_customizations/models/product.py
@@ -8,14 +8,33 @@ class ProductTemplate(models.Model):
     standard_bom = fields.Boolean('Having Standard BOM', default=False)
     non_standard_bom = fields.Boolean('Not having Standard BOM', default=False)
 
-    @api.onchange('standard_bom')
-    def _onchange_standard_bom(self):
-        if self.standard_bom:
-            self.non_standard_bom = False
+    # @api.onchange('standard_bom')
+    # def _onchange_standard_bom(self):
+    #     for product in self:
+    #         print(product.standard_bom)
+    #         if product.standard_bom:
+    #             product.non_standard_bom = False
+    #
+    # @api.onchange('non_standard_bom')
+    # def _onchange_non_standard_bom(self):
+    #     for product in self:
+    #         if product.non_standard_bom:
+    #             product.standard_bom = False
 
-    @api.onchange('non_standard_bom')
-    def _onchange_non_standard_bom(self):
-        if self.non_standard_bom:
-            self.standard_bom = False
-    
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+    #
+    # @api.onchange('standard_bom')
+    # def _onchange_standard_bom(self):
+    #     for product in self:
+    #         if product.standard_bom:
+    #             product.non_standard_bom = False
+    #
+    # @api.onchange('non_standard_bom')
+    # def _onchange_non_standard_bom(self):
+    #     for product in self:
+    #         if product.non_standard_bom:
+    #             product.standard_bom = False
+
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
